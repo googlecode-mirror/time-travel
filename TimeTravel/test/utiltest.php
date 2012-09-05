3b6e911398e7a1f97e74bf3c2e4fd3ed8ef2ed77
sabside <sabside@gmail.com>
2012-09-05 20:45:12 +0000

    dropbox implementation

--- TimeTravel/test/utiltest.php
+++ TimeTravel/test/utiltest.php
@@ -1,8 +1,23 @@
 <?php
 require_once(dirname(dirname(__FILE__)) .'/util.php');
+require_once(dirname(dirname(__FILE__)) .'/services/DropboxService.php');
+require_once(dirname(dirname(__FILE__)) .'/bootstrap.php');
 
-$myString = 'Innocent Pereira <innocent.pereira@gmail.com>';
+$OAuth = new \Dropbox\OAuth\Consumer\Curl($key, $secret, $storage, $callback);
+$dropbox = new \Dropbox\API($OAuth);
 
-echo Util::getSourceName($myString);
+$service = new DropboxService();
+
+$parameters = array();
+
+$folderList = $service->fetchFolders($parameters, $dropbox);
+$numOfFolders = sizeof($folderList);
+
+$service->readDirs('/Software', $dropbox);
+
+/* for ($i = 0; $i < $numOfFolders; $i++) {
+	$folder = $folderList[$i]->{'path'};
+	$service->readDirs($folder, $dropbox);
+} */
 
 ?>
